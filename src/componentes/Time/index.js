147fe0c7ee067e7f77d082914f0e7e57ee396c65
Daniel <danielmster56@gmail.com>
2023-09-11 17:34:15 -0300

    instalando dependencias

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -6,9 +6,8 @@ const Time = ({ time, colaboradores }) => {
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => {
-                    console.log(<Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={()=>{}}/>);
-                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={()=>{}}/>
+                {colaboradores.map((colaborador, indice, aoDeletar) => {
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar}/>
                 }
             )}
             </div>
