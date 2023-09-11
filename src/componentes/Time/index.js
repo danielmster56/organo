6f9de131e44dca3d9e0cb3c95f2abf0ecd76de66
Daniel <danielmster56@gmail.com>
2023-09-11 11:30:06 -0300

    iniciando projeto Organo

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,14 +3,16 @@ import './time.css'
 
 const Time = ({ time, colaboradores }) => {
     return (
-
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
+                {colaboradores.map((colaborador, indice) => {
+                    console.log(<Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={()=>{}}/>);
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={()=>{}}/>
+                }
+            )}
             </div>
         </section>
-
     )
 }
 
