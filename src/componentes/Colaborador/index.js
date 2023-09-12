07a83851e714dc612c7045d495ca226e43fbd8f5
Daniel <danielmster56@gmail.com>
2023-09-12 11:21:44 -0300

    emplementando controles de cor e pacote hex-to-rgba

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -15,22 +15,3 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
 }
 
 export default Colaborador
-
-// import { AiFillCloseCircle } from 'react-icons/ai';
-// import './colaborador.css'
-
-// const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
-//     return (
-//         <div className="colaborador">
-//         <AiFillCloseCircle size={25} className="deletar" onClick={() => aoDeletar}/>
-//         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-//             <img src={colaborador.imagem} alt={colaborador.nome} />
-//         </div>
-//         <div className="rodape">
-//             <h4>{colaborador.nome}</h4>
-//             <h5>{colaborador.cargo}</h5>
-//         </div>
-//     </div>)
-// }
-
-// export default Colaborador
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,12 +1,13 @@
 import Colaborador from '../Colaborador'
 import './time.css'
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+            <input value={time.cor} onChange={evento=>mudarCor(evento.target.value, time.nome)} type='color' className='input-cor' />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar} />)}
+                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar} />)}
             </div>
         </section>
 
