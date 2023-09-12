91f59e7be13db0ece2bf2976fd23b19d2b301001
Daniel <danielmster56@gmail.com>
2023-09-12 19:08:31 -0300

    emplementando o card de criação de time com nome e cor

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -7,7 +7,7 @@ const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.5') }}>
             <input value={time.cor} onChange={evento=>mudarCor(evento.target.value, time.id)} type='color' className='input-cor' />
-            <h3 style={{ borderColor: time.cor }}>{time.id}</h3>
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar} />)}
             </div>
